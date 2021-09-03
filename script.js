b3be870930ede1d2ed3fec54b2b98cb7a2959e86
ShokhrukhTurgunov <77617034+ShokhrukhTurgunov@users.noreply.github.com>
2021-09-03 09:43:30 +0500

    add segmental control files

---
+++ script.js
@@ -0,0 +1,14 @@
+// function checking(value) {
+//     document.getElementById(value).checked = true;
+//     document.getElementById("sorting-method").value = value;
+// }
+
+
+// let inputs = document.querySelectorAll('input');
+
+
+// inputs.forEach( (inp) => {
+//     inp.addEventListener('click', (evt) => {
+//         evt.classList.toggle('checked');
+//     })
+// })
